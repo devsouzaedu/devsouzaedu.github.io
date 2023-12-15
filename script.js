3095ebab44715cd1f4b254b86fe760c9d5777ac6
Eduardo Souza <dev.souzaedu@gmail.com>
2023-12-15 14:55:15 -0300

    v10.2

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function scrollToPortfolio() {
     document.getElementById('Ptn').scrollIntoView({ behavior: 'smooth' });
 }
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function scrollToPortfolio() {
+    document.getElementById('portfolio').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAboutMe() {
+    document.getElementById('aboutme').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToContact() {
+    document.getElementById('form1').scrollIntoView({ behavior: 'smooth' });
+}
